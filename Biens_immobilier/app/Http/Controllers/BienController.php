fb040e0530a2cfd33495f68f2aa6439be3a1f0ad
Oumyf <falladiaraoumy@gmail.com>
2024-06-03 16:44:51 +0000

    ajout de biens immobiliers choisissant la catégorie et l'utilisateur

---
+++ Biens_immobilier/app/Http/Controllers/BienController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Bien;
+use App\Models\User;
+use App\Models\Categorie;
+use Illuminate\Http\Request;
+
+class BienController extends Controller
+{
+    public function ajouterBien() {
+        $categories = Categorie::all(); // Récupère toutes les catégories de la base de données
+        $users = User::all(); // Récupère toutes les catégories de la base de données
+        return view('bien.ajouterBien',compact('categories','users'));
+    }
+
+    public function ajouterBienTraitement(Request $request){
+        $validatedData = $request->validate([
+            'nom' => 'required|string|max:255',
+            'description' => 'required|string',
+            'adresse' => 'required|string',
+            'statut' => 'required|in:occupe,pas_occupe',
+            'image' => 'required',
+            'user_id' => 'required|exists:users,id',
+            'categorie_id' => 'required|exists:categories,id',
+
+        ]);
+    
+                // Initialisation de la variable pour le chemin de l'image
+                $image = null;
+                // Vérifier si un fichier image est uploadé
+                if ($request->hasFile('image')) {
+                    // Stocker l'image dans le répertoire 'public/blog'
+                    $chemin_image = $request->file('image')->store('public/blog');
+        
+                    // Vérifier si le chemin de l'image est bien généré
+                    if (!$chemin_image) {
+                        return redirect()->back()->with('error', "Erreur lors du téléchargement de l'image.");
+                    }
+        
+                    // Récupérer le nom du fichier de limage
+                    $image = basename($chemin_image);
+                }
+        
+    
+        Bien::create($validatedData);
+    
+        return redirect()->route('Bien.ajouterBien')->with('status', "Le bien a été ajouté avec succès");
+
+    }
+    
+}
--- Biens_immobilier/routes/web.php
+++ Biens_immobilier/routes/web.php
@@ -1,7 +1,29 @@
 <?php
 
+use App\Http\Controllers\BienController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/bien' , [BienController::class,'ListeBien']);
+Route::get('/ajouter' , [BienController::class,'ajouterBien']);
+Route::post('/ajouterBien/Traitement' , [BienController::class,'ajouterBienTraitement'])->name('Bien.ajouterBien
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+');
+
